03e85c8b06cbf7f6d134027a405be700f9e7a00a
xetob <xetob1122@gmail.com>
2024-12-22 18:04:17 +0300

    disable log

--- make11.sh
+++ make11.sh
@@ -20,5 +20,5 @@ ln -s  /apex/com.android.vndk.v29/lib64 $systempath/lib64/vndk-sp-29
 ln -s  /apex/com.android.vndk.v28/lib64 $systempath/lib64/vndk-sp-28
 
 # Extract VNDK apex to system
-7z x -y $thispath/11/com.android.vndk.v28.apex.7z -o$systempath/system_ext/apex/
-7z x -y $thispath/11/com.android.vndk.v29.apex.7z -o$systempath/system_ext/apex/
+7z x -y $thispath/11/com.android.vndk.v28.apex.7z -o$systempath/system_ext/apex/ -bso0 -bsp0
+7z x -y $thispath/11/com.android.vndk.v29.apex.7z -o$systempath/system_ext/apex/ -bso0 -bsp0
