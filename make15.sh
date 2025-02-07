54d72e869a1abcca46681168c40e5e8b9ddc894e
xetob <xetob1122@gmail.com>
2025-02-07 18:51:24 +0300

    14-15 support

---
+++ make15.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+systempath=$1
+thispath=`cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd`
+
+# Make VNDK symlinks
+rm -rf $systempath/lib/vndk-34 $systempath/lib/vndk-sp-34
+rm -rf $systempath/lib/vndk-33 $systempath/lib/vndk-sp-33
+rm -rf $systempath/lib/vndk-32 $systempath/lib/vndk-sp-32
+rm -rf $systempath/lib/vndk-31 $systempath/lib/vndk-sp-31
+rm -rf $systempath/lib/vndk-30 $systempath/lib/vndk-sp-30
+rm -rf $systempath/lib/vndk-29 $systempath/lib/vndk-sp-29
+rm -rf $systempath/lib/vndk-28 $systempath/lib/vndk-sp-28
+rm -rf $systempath/lib64/vndk-34 $systempath/lib64/vndk-sp-34
+rm -rf $systempath/lib64/vndk-33 $systempath/lib64/vndk-sp-33
+rm -rf $systempath/lib64/vndk-32 $systempath/lib64/vndk-sp-32
+rm -rf $systempath/lib64/vndk-31 $systempath/lib64/vndk-sp-31
+rm -rf $systempath/lib64/vndk-30 $systempath/lib64/vndk-sp-30
+rm -rf $systempath/lib64/vndk-29 $systempath/lib64/vndk-sp-29
+rm -rf $systempath/lib64/vndk-28 $systempath/lib64/vndk-sp-28
+
+ln -s  /apex/com.android.vndk.v34/lib $systempath/lib/vndk-34
+ln -s  /apex/com.android.vndk.v33/lib $systempath/lib/vndk-33
+ln -s  /apex/com.android.vndk.v32/lib $systempath/lib/vndk-32
+ln -s  /apex/com.android.vndk.v31/lib $systempath/lib/vndk-31
+ln -s  /apex/com.android.vndk.v30/lib $systempath/lib/vndk-30
+ln -s  /apex/com.android.vndk.v29/lib $systempath/lib/vndk-29
+ln -s  /apex/com.android.vndk.v28/lib $systempath/lib/vndk-28
+ln -s  /apex/com.android.vndk.v34/lib $systempath/lib/vndk-sp-34
+ln -s  /apex/com.android.vndk.v33/lib $systempath/lib/vndk-sp-33
+ln -s  /apex/com.android.vndk.v32/lib $systempath/lib/vndk-sp-32
+ln -s  /apex/com.android.vndk.v31/lib $systempath/lib/vndk-sp-31
+ln -s  /apex/com.android.vndk.v30/lib $systempath/lib/vndk-sp-30
+ln -s  /apex/com.android.vndk.v29/lib $systempath/lib/vndk-sp-29
+ln -s  /apex/com.android.vndk.v28/lib $systempath/lib/vndk-sp-28
+
+ln -s  /apex/com.android.vndk.v34/lib64 $systempath/lib64/vndk-34
+ln -s  /apex/com.android.vndk.v33/lib64 $systempath/lib64/vndk-33
+ln -s  /apex/com.android.vndk.v32/lib64 $systempath/lib64/vndk-32
+ln -s  /apex/com.android.vndk.v31/lib64 $systempath/lib64/vndk-31
+ln -s  /apex/com.android.vndk.v30/lib64 $systempath/lib64/vndk-30
+ln -s  /apex/com.android.vndk.v29/lib64 $systempath/lib64/vndk-29
+ln -s  /apex/com.android.vndk.v28/lib64 $systempath/lib64/vndk-28
+ln -s  /apex/com.android.vndk.v34/lib64 $systempath/lib64/vndk-sp-34
+ln -s  /apex/com.android.vndk.v33/lib64 $systempath/lib64/vndk-sp-33
+ln -s  /apex/com.android.vndk.v32/lib64 $systempath/lib64/vndk-sp-32
+ln -s  /apex/com.android.vndk.v31/lib64 $systempath/lib64/vndk-sp-31
+ln -s  /apex/com.android.vndk.v30/lib64 $systempath/lib64/vndk-sp-30
+ln -s  /apex/com.android.vndk.v29/lib64 $systempath/lib64/vndk-sp-29
+ln -s  /apex/com.android.vndk.v28/lib64 $systempath/lib64/vndk-sp-28
+
+# Extract VNDK apex to system
+7z x -y $thispath/15/apex_15.7z -o$systempath/apex/ -bso0 -bsp0
