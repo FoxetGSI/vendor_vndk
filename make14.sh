d8f9ae3700533b21771827b55dcd6b9155b345e8
xetob <xetob1122@gmail.com>
2025-02-09 21:53:41 +0300

    14: кь 28-29

--- make14.sh
+++ make14.sh
@@ -24,30 +24,22 @@ ln -s  /apex/com.android.vndk.v33/lib $systempath/lib/vndk-33
 ln -s  /apex/com.android.vndk.v32/lib $systempath/lib/vndk-32
 ln -s  /apex/com.android.vndk.v31/lib $systempath/lib/vndk-31
 ln -s  /apex/com.android.vndk.v30/lib $systempath/lib/vndk-30
-ln -s  /apex/com.android.vndk.v29/lib $systempath/lib/vndk-29
-ln -s  /apex/com.android.vndk.v28/lib $systempath/lib/vndk-28
 ln -s  /apex/com.android.vndk.v34/lib $systempath/lib/vndk-sp-34
 ln -s  /apex/com.android.vndk.v33/lib $systempath/lib/vndk-sp-33
 ln -s  /apex/com.android.vndk.v32/lib $systempath/lib/vndk-sp-32
 ln -s  /apex/com.android.vndk.v31/lib $systempath/lib/vndk-sp-31
 ln -s  /apex/com.android.vndk.v30/lib $systempath/lib/vndk-sp-30
-ln -s  /apex/com.android.vndk.v29/lib $systempath/lib/vndk-sp-29
-ln -s  /apex/com.android.vndk.v28/lib $systempath/lib/vndk-sp-28
 
 ln -s  /apex/com.android.vndk.v34/lib64 $systempath/lib64/vndk-34
 ln -s  /apex/com.android.vndk.v33/lib64 $systempath/lib64/vndk-33
 ln -s  /apex/com.android.vndk.v32/lib64 $systempath/lib64/vndk-32
 ln -s  /apex/com.android.vndk.v31/lib64 $systempath/lib64/vndk-31
 ln -s  /apex/com.android.vndk.v30/lib64 $systempath/lib64/vndk-30
-ln -s  /apex/com.android.vndk.v29/lib64 $systempath/lib64/vndk-29
-ln -s  /apex/com.android.vndk.v28/lib64 $systempath/lib64/vndk-28
 ln -s  /apex/com.android.vndk.v34/lib64 $systempath/lib64/vndk-sp-34
 ln -s  /apex/com.android.vndk.v33/lib64 $systempath/lib64/vndk-sp-33
 ln -s  /apex/com.android.vndk.v32/lib64 $systempath/lib64/vndk-sp-32
 ln -s  /apex/com.android.vndk.v31/lib64 $systempath/lib64/vndk-sp-31
 ln -s  /apex/com.android.vndk.v30/lib64 $systempath/lib64/vndk-sp-30
-ln -s  /apex/com.android.vndk.v29/lib64 $systempath/lib64/vndk-sp-29
-ln -s  /apex/com.android.vndk.v28/lib64 $systempath/lib64/vndk-sp-28
 
 # Extract VNDK apex to system
 7z x -y $thispath/14/apex_14.7z -o$systempath/apex/ -bso0 -bsp0
